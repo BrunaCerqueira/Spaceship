107279700ffecf3667caa3b778a0bce9f09af879
BrunaCerqueira <133819587+BrunaCerqueira@users.noreply.github.com>
2024-11-06 10:00:43 -0300

    Add prefabs + cam mov
    Adjusted player move to consider local position
    Added Bullet Prefab
    Added Screen movement to the right.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,22 +14,22 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && transform.position.x > -7)
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && transform.localPosition.x > -8)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
 
-        if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && transform.position.x < 9)
+        if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && transform.localPosition.x < 9)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed * (-1));
         }
 
-        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && transform.position.y < 5)
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && transform.localPosition.y < 5)
         {
             transform.Translate(Vector3.up * Time.deltaTime * speed);
         }
 
-        if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && transform.position.y > -5)
+        if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && transform.localPosition.y > -5)
         {
             transform.Translate(Vector3.down * Time.deltaTime * speed);
         }
