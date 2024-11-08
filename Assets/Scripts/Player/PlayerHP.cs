8150ea226b46904dd9434eea0e4141adc3fb053d
BrunaCerqueira <133819587+BrunaCerqueira@users.noreply.github.com>
2024-11-08 14:45:41 -0300

    Add Game over Scene
    Add game over condition
    Add try again buttom

--- Assets/Scripts/Player/PlayerHP.cs
+++ Assets/Scripts/Player/PlayerHP.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerHP : MonoBehaviour
 {
@@ -16,7 +17,11 @@ public class PlayerHP : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (HP == 0)
+        {
+            FullHealth();
+            SceneManager.LoadScene("SceneDerrota");
+        }
     }
     public void OnCollisionEnter(Collision collision)
     {
@@ -27,5 +32,10 @@ public class PlayerHP : MonoBehaviour
             life.text = "Health: " + HP.ToString();
         }
         Debug.Log("Encostei em alguma coisa" + collision.gameObject.tag);
+    }
+
+    public void FullHealth()
+    {
+        HP += 3;
     }
 }
